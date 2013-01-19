c81ac91ac94799f0c6248f41655625b9af1df44b
Avi Bryant <avi@etsy.com>
2013-01-18 22:11:01 -0800

    Simple demo job

---
+++ src/main/java/io/sizzling/examples/IntSumJob.java
@@ -0,0 +1,15 @@
+package io.sizzling.examples;
+
+import io.sizzling.*;
+
+public class IntSumJob extends Job<String> {
+  Table<Integer> sum = table(new IntSum(), "sum");
+
+  public void process(String line) {
+    int i = Integer.parseInt(line);
+    if(i % 2 == 0)
+      sum.emit("even", i);
+    else
+      sum.emit("odd", i);
+  }
+}
